1ae962460e73d68c9c0d80a2bee2750dafd83f83
Lik Edurad <feft99@mail.ru>
2019-04-23 03:51:48 +0600

    navbar color changed

--- views/main.php
+++ views/main.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Киноночь Алматы</title>
+	<meta http-equiv="Cache-Control" content="no-cache">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="description" content=" ">
 	<meta name="keywords" content=" ">
