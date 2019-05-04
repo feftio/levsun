c34eb939fcab5c9f2c56acd3af2464dd1e27d8c3
Lik Edurad <feft99@mail.ru>
2019-05-05 05:26:42 +0600

    new block in main.php

--- views/main.php
+++ views/main.php
@@ -18,33 +18,23 @@
 				</div>
 			</section>
 		</header>
+		<section class="form" id="form">
+			<div class="form__block">
+
+			</div>
+		</section>
 		<section class="info" id="info">
-			<div class="info__headline">
-				<h1 class="info__headline__h1">Inforamation about!</h1>
-				<p class="info__healine__p">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			<div class="info__text">
+				<h2>Buy the ticket!</h2>
+				<p>asdaijojafoasofjsoafjasjfosa</p>
 			</div>
-			<div class="info__wrapper">
-				<div class="info__block img__glasses">
-					<div class="info__circle"></div>
-					<h2 class="info__h2">One part!</h3>
-					<p class="info__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-				</div>
-				<div class="info__block img__glasses">
-					<div class="info__circle"></div>
-					<h2 class="info__h2">Two part!</h3>
-					<p class="info__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-				</div>
-				<div class="info__block img__glasses">
-					<div class="info__circle"></div>
-					<h2 class="info__h2">Three part!</h3>
-					<p class="info__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-				</div>
-				<div class="info__block img__glasses">
-					<div class="info__circle"></div>
-					<h2 class="info__h2">Four part!</h3>
-					<p class="info__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-				</div>
+			<div class="info__blocks">
+				<div class="info__block info__one"></div>
+				<div class="info__block info__two"></div>
+				<div class="info__block info__three"></div>
+				<div class="info__block info__four"></div>
 			</div>
+			
 		</section>
 
 		<?php View::render('views/parts', 'footer.php') ?>
