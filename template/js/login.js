48e79f3791cfd0a8dd0a60442f2383c6863b5c97
Lik Edurad <feft99@mail.ru>
2019-05-09 02:59:57 +0600

    done /login

---
+++ template/js/login.js
@@ -0,0 +1,90 @@
+var span = document.querySelectorAll(".eye");
+span[0].addEventListener("click", showPW);
+span[1].addEventListener("click", showPW);
+
+function validation()                                    
+{ 
+    var name = document.forms["RegForm"]["Name"];               
+    var email = document.forms["RegForm"]["EMail"];    
+    var phone = document.forms["RegForm"]["Telephone"];  
+    var what =  document.forms["RegForm"]["Subject"];  
+    var password = document.forms["RegForm"]["Password"];  
+    var address = document.forms["RegForm"]["Address"];  
+   
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    } 
+   
+    if (address.value == "")                               
+    { 
+        window.alert("Please enter your address."); 
+        name.focus(); 
+        return false; 
+    } 
+       
+    if (email.value == "")                                   
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (phone.value == "")                           
+    { 
+        window.alert("Please enter your telephone number."); 
+        phone.focus(); 
+        return false; 
+    } 
+   
+    if (password.value == "")                        
+    { 
+        window.alert("Please enter your password"); 
+        password.focus(); 
+        return false; 
+    } 
+   
+    if (what.selectedIndex < 1)                  
+    { 
+        alert("Please enter your course."); 
+        what.focus(); 
+        return false; 
+    } 
+   
+    return true; 
+}
+
+function showPW()
+{
+    var span = document.querySelectorAll(".eye");
+        input = document.getElementById("password");
+
+    span[0].classList.toggle("eye-active");
+    span[1].classList.toggle("eye-active");
+
+    if (span[1].classList.contains('eye-active'))
+    {
+        input.setAttribute("type", "text");
+    }
+
+    if (span[0].classList.contains('eye-active'))
+    {
+        input.setAttribute('type', 'password');
+    }
+}
--- views/login.php
+++ views/login.php
@@ -4,27 +4,36 @@
 <?php View::render('views/parts', 'head.php') ?>
 
 <body>
-	<div class="wrapper">
-		<div class="line"><a href="/"><i class="fas fa-arrow-left"></i></a></div>
-		<div class="login">
-			<form class="login__form" action="POST">
-				<a href="/"><img src="/template/image/logoCN1.png" alt="placeholder+image" width="80" height="80"></a>
-				<span>Вход</span>
-				<div class="login__block">
-					<input class="form__input" type="text" name="login">
-					<span class="focus-input100" data-placeholder="Email"></span>
-				</div>
-				<div class="password__block">
-					<span class="show-password"><i class="fa fa-eye eye" aria-hidden="true"></i></span>
-					<input class="form__input" type="password" name="password">
-					<span class="focus-input100"></span>
-				</div>
-			</form>
-		</div>
-		<div class="reg">
-			<form class="reg__form" action="POST">
-			</form>
+	<div class="container">
+		<div class="big-image"></div>
+		<div class="wrapper">
+			<a class="arrow-back" href="/"><i class="fa fa-arrow-left"></i></a>
+			<div class="form__wrapper">
+				<form class="form" method="post" onsubmit="validation(this)">
+					<div class="logo__circle"><a class="logo__image" href="/"><img src="/template/image/logoCN1.png" alt="placeholder+image" width="70" height="70"></a></div>
+					<span class="form__welcome">Добро пожаловать!</span>
+					<div class="login out-input">	
+						<input class="input-style" type="text" name="login" placeholder="Логин">
+						<span class="focus-input" data-placeholder="Логин"></span>
+					</div>
+					<div class="password out-input">
+						<input class="input-style" id="password" type="password" name="password" placeholder="Пароль">
+						<span class="eye eye-active"><i class="fa fa-eye" aria-hidden="true"></i></span>
+						<span class="eye"><i class="fa fa-eye-slash" aria-hidden="true"></i></span>
+						<span class="focus-input" data-placeholder="Пароль"></span>
+					</div>
+					<div class="sign-in">
+						<button class="button">Войти</button>		
+					</div>
+					<div></div>
+					<div class="pop-reg">
+						<span>У вас нет аккаунта? </span><label>Зарегистрироваться</label>
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
+	<script type="text/javascript" src="/template/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="/template/js/login.js"></script>
 </body>
 </html>
